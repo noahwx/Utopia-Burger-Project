ccc34780781fe8407e8d2aa75880921e047c9681
noahwx <noahfontenette@outlook.com>
2023-03-07 00:34:14 -0600

    0.0.003

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import { Header } from './components/header';
+import './index.css';
+import Main from './Main';
+
+// Version 0.0.003
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+}
+
+export default App;
